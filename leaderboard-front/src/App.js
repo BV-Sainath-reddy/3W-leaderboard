4eefd9d75dfec6665208e69df682098dca18cff2
BV-Sainath-reddy <reddysainath268@gmail.com>
2025-07-17 22:04:07 +0530

    Add files via upload

---
+++ leaderboard-front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Leaderboard from './components/Leaderboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Leaderboard />
+    </div>
+  );
+}
+
+export default App;
